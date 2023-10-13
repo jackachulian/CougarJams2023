c0c67e380cc01610c5b2fb505b3a6b906a2196ac
Jackachulian <jackachulian@gmail.com>
2023-10-13 17:10:28 -0500

    Screen edge fix

--- CougarJams2023/Assets/Scripts/ScreenEdge.cs
+++ CougarJams2023/Assets/Scripts/ScreenEdge.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ScreenEdge : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name != "Player") return;
         PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
         health.TakeDamage(999);
     }
