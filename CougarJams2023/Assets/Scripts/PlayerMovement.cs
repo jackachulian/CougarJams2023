a0af493adcebd9e58f7b6ac56229449dd73a3c76
Jackachulian <jackachulian@gmail.com>
2023-10-13 15:11:54 -0500

    Basic player movement

--- CougarJams2023/Assets/Scripts/PlayerMovement.cs
+++ CougarJams2023/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalMovement > 0) {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        } else if (horizontalMovement < 0) {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        } else {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
     }
 }
