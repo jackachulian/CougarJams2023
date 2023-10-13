dbd2623f238947e5f140f7e1d01099d4655d3008
Jackachulian <jackachulian@gmail.com>
2023-10-13 15:33:20 -0500

    player jump

--- CougarJams2023/Assets/Scripts/PlayerMovement.cs
+++ CougarJams2023/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
 
+    [SerializeField] private float jumpSpeed = 5f;
+
+    [SerializeField] private LayerMask groundLayerMask;
+
     private Rigidbody2D rb;
+
+
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +26,41 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        if (horizontalMovement > 0) {
+        if (horizontalMovement > 0) 
+        {
             rb.velocity = new Vector2(speed, rb.velocity.y);
         } else if (horizontalMovement < 0) {
             rb.velocity = new Vector2(-speed, rb.velocity.y);
         } else {
             rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+
+        bool jumpInput = Input.GetButtonDown("Jump");
+
+        if (jumpInput && isGrounded) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+
+        // after calculations, set to false, will be set by collisions if true
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        HandleCollision(col);
+    }
+
+    private void OnCollisionStay2D(Collision2D col) {
+        // will be set to true if in contact with any ground colliders
+        HandleCollision(col);
+    }
+
+    void HandleCollision(Collision2D col) {
+        // Will be true if collision's layer is contained in the ground layer mask on this obj
+		if((groundLayerMask & (1 << col.gameObject.layer)) != 0)
+		{
+			isGrounded = true;
         }
     }
 }
