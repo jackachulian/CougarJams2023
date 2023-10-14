7de681bef1aac1aa9d086b8ff8dcede8ecc6a42e
NickRichmond2005 <thedeadkindo@gmail.com>
2023-10-13 22:08:32 -0500

    immunity

--- CougarJams2023/Assets/Scripts/ScreenEdge.cs
+++ CougarJams2023/Assets/Scripts/ScreenEdge.cs
@@ -4,6 +4,7 @@ public class ScreenEdge : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name != "Player") return;
         PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
+        Time.timeScale = 0;
         health.TakeDamage(999);
     }
 }
