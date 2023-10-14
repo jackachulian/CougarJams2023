19c04d97cce3848c6154f6f61ea60f368d36da7f
Brandon Underwood <coolbrandon04@gmail.com>
2023-10-13 23:56:37 -0500

    my changes

--- CougarJams2023/Assets/Scripts/ScreenEdge.cs
+++ CougarJams2023/Assets/Scripts/ScreenEdge.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using UnityEngine;
 
 public class ScreenEdge : MonoBehaviour {
+    //[SerializeField] private float healthCooldown = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name != "Player") return;
         PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
         Time.timeScale = 0;
+        StartCoroutine(HealthTimer(health));
+    }
+
+    
+    private IEnumerator HealthTimer(PlayerHealth health)
+    {
         while (health.health != 0)
         {
+            yield return new WaitForSecondsRealtime(0.5f);
             health.health -= 1;
         }
     }
+
+
 }
