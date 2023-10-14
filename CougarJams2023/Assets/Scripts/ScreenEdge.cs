90b54c09b8461ae2066e8747472afb0b96d53035
NickRichmond2005 <thedeadkindo@gmail.com>
2023-10-14 07:46:14 -0500

    Hopes and Dream

--- CougarJams2023/Assets/Scripts/ScreenEdge.cs
+++ CougarJams2023/Assets/Scripts/ScreenEdge.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScreenEdge : MonoBehaviour {
 
     public GameObject GameOver;
+    public bool door;
 
     private void Awake()
     {
@@ -12,18 +14,19 @@ public class ScreenEdge : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        ScrollingScreen begin = FindFirstObjectByType<ScrollingScreen>();
-        if (other.gameObject.name != "Player") return;
-        PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
-        if (begin)
-        {
+            ScrollingScreen begin = FindFirstObjectByType<ScrollingScreen>();
+            if (other.gameObject.name != "Player") return;
+            PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
+            if (begin)
+            {
+                Time.timeScale = 0;
+                StartCoroutine(HealthTimer(health));
+                GameOver.SetActive(true);
+            }
             Time.timeScale = 0;
             StartCoroutine(HealthTimer(health));
-            GameOver.SetActive(true);
-        }
-        Time.timeScale = 0;
-        StartCoroutine(HealthTimer(health));
-        GameOver.GetComponent<Canvas>().enabled = true;
+            GameOver.GetComponent<Canvas>().enabled = true;
+        
     }
     private IEnumerator HealthTimer(PlayerHealth health)
     {
