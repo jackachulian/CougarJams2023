6e3a1a850e8aadbe3179e1f1c37aba6769ab14dd
NickRichmond2005 <thedeadkindo@gmail.com>
2023-10-14 00:01:44 -0500

    Display Game Over Screen

--- CougarJams2023/Assets/Scripts/ScreenEdge.cs
+++ CougarJams2023/Assets/Scripts/ScreenEdge.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using UnityEngine;
 
 public class ScreenEdge : MonoBehaviour {
+    public GameObject GameOver;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name != "Player") return;
         PlayerHealth health = FindFirstObjectByType<PlayerHealth>();
         Time.timeScale = 0;
         StartCoroutine(HealthTimer(health));
+        GameOver.SetActive(true);
     }
     private IEnumerator HealthTimer(PlayerHealth health)
     {
