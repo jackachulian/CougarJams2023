8c8caaa23383dcc24d50b9e4c22ab5d85caa8205
NickRichmond2005 <thedeadkindo@gmail.com>
2023-10-14 15:26:21 -0500

    After 1st boss

--- CougarJams2023/Assets/Scripts/TreasureSuprize.cs
+++ CougarJams2023/Assets/Scripts/TreasureSuprize.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class TreasureSuprize : MonoBehaviour
 {
-    public GameObject prefab;
-    private Vector2 spawnposition;
+    public GameObject gameObject;
+    public GameObject gameObject1;
+    public GameObject gameObject2;
+    
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            Instantiate(prefab, new Vector2(-73f,1.81f), Quaternion.identity);
-            Instantiate(prefab, new Vector2(-60.3f, 1.81f), Quaternion.identity);
-            Instantiate(prefab, new Vector2(-66.7f, 1.81f), Quaternion.identity);
+            gameObject.SetActive(true);
+            gameObject1.SetActive(true);
+            gameObject2.SetActive(true);
         }
     }
 
