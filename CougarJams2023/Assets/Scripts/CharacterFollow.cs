9206abf88694e039e793e37e32037943f8ea8cfb
Brandon Underwood <coolbrandon04@gmail.com>
2023-10-14 06:08:16 -0500

    Camera Final Fix

--- CougarJams2023/Assets/Scripts/CharacterFollow.cs
+++ CougarJams2023/Assets/Scripts/CharacterFollow.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class CharacterFollow : MonoBehaviour
 {
     public Transform player;
+    public float minX;
+    public float maxX;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        if (player.transform.position.x >= minX && player.transform.position.x <= maxX)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
     }
 }
